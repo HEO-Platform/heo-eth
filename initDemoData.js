bbd6111001eee3472af7b0ad63f34f536bb5f11d
Greg Solovyev <fiddlestring@gmail.com>
2021-02-19 18:28:55 -0800

    Do not create demo campaigns

--- initDemoData.js
+++ initDemoData.js
@@ -21,17 +21,27 @@ module.exports = async (callback) => {
     console.log("Deployed stable coin TUSD at " + iTestCoin.address);
     //set initial values
     let accounts = await web3.eth.getAccounts();
+    let ownerAccount = accounts[0];
+    let charityAccount1 = accounts[1];
+    let charityAccount2 = accounts[2];
     await iTestCoin.transfer("0x748351f954Af3C95a41b88ba7563453Ab98eA085", web3.utils.toWei("10000"));
     await iTestCoin.transfer("0x02C364e8048C60c980d4C1abb9918f66D716d603", web3.utils.toWei("10000"));
     await iTestCoin.transfer("0x6CFe06BCC19444b90fe5f8729eb619c51Fcb7e3A", web3.utils.toWei("10000"));
-    let ownerAccount = accounts[0];
+
     await iRegistry.setFactory(iCampaignFactory.address);
     await iToken.addMinter(iDistribution.address, {from: ownerAccount});
     await iToken.addBurner(iCampaignFactory.address, {from: ownerAccount});
-    await iPriceOracle.setPrice(iTestCoin.address, web3.utils.toWei("10"));
+    await iPriceOracle.setPrice(iTestCoin.address, web3.utils.toWei("1"));
 
+
+    await iDistribution.distribute(charityAccount1, web3.utils.toWei("100"), {from: ownerAccount});
+    let balance1 = await iToken.balanceOf(charityAccount1);
+    console.log(`Chartity account 1 (${charityAccount1}) has balance of ${web3.utils.fromWei(balance1.toString())} HEO`);
+    await iDistribution.distribute(charityAccount2, web3.utils.toWei("100"), {from: ownerAccount});
+    let balance2 = await iToken.balanceOf(charityAccount2);
+    console.log(`Chartity account 1 (${charityAccount2}) has balance of ${web3.utils.fromWei(balance2.toString())} HEO`);
     //deploy demo campaigns
-    let charityAccount1 = accounts[1];
+/*    let charityAccount1 = accounts[1];
     let charityAccount2 = accounts[2];
     await iDistribution.distribute(charityAccount1, web3.utils.toWei("100"), {from: ownerAccount});
     let balance1 = await iToken.balanceOf(charityAccount1);
@@ -47,6 +57,6 @@ module.exports = async (callback) => {
     await iCampaignFactory.createCampaign(web3.utils.toWei("100"), web3.utils.toWei("0"),
         iTestCoin.address, "https://heometa.s3.us-east-2.amazonaws.com/json/campaign1.json", {from: charityAccount2});
     myCampaigns = await iRegistry.myCampaigns.call({from: charityAccount2});
-    console.log(`Account ${charityAccount2} deployed campaign at ${myCampaigns[0]}`);
+    console.log(`Account ${charityAccount2} deployed campaign at ${myCampaigns[0]}`);*/
     callback();
 }
