949b5ea0d9b13d3ee8f1e2547407ab3959f0ae75
Greg Solovyev <fiddlestring@gmail.com>
2021-11-05 12:15:53 -0700

    Log txn hash and cost for transfering ownership of registry

--- migrations/6_deploy_registry.js
+++ migrations/6_deploy_registry.js
@@ -6,7 +6,12 @@ module.exports = async function(deployer, network, accounts) {
         const iHEODao = await HEODAO.deployed();
         await deployer.deploy(HEOCampaignRegistry, iHEODao.address);
         const iRegistry = await HEOCampaignRegistry.deployed();
-        await iRegistry.transferOwnership(iHEODao.address);
         console.log(`HEOCampaignRegistry address on ${network}: ${iRegistry.address}`);
+
+        let txReceipt = await iRegistry.transferOwnership(iHEODao.address);
+        console.log(`transferOwnership transaction cost: ${txReceipt.receipt.gasUsed}`);
+        console.log(`transferOwnership transaction hash: ${txReceipt.receipt.transactionHash}`);
+        console.log(`transferOwnership transaction block hash: ${txReceipt.receipt.blockHash}`);
+
     }
 }
