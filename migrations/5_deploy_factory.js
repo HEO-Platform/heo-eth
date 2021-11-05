856a9224f718c3f9d1c2c1b580cf2e57fd04d919
Greg Solovyev <fiddlestring@gmail.com>
2021-11-05 12:14:36 -0700

    Display txn hash and cost for transferOwnership

--- migrations/5_deploy_factory.js
+++ migrations/5_deploy_factory.js
@@ -6,7 +6,11 @@ module.exports = async function(deployer, network, accounts) {
         const iHEODao = await HEODAO.deployed();
         await deployer.deploy(HEOCampaignFactory, iHEODao.address);
         const iCampaignFactory = await HEOCampaignFactory.deployed();
-        await iCampaignFactory.transferOwnership(iHEODao.address);
         console.log(`HEOCampaignFactory address on ${network}: ${HEOCampaignFactory.address}`);
+
+        let txReceipt = await iCampaignFactory.transferOwnership(iHEODao.address);
+        console.log(`transferOwnership transaction cost: ${txReceipt.receipt.gasUsed}`);
+        console.log(`transferOwnership transaction hash: ${txReceipt.receipt.transactionHash}`);
+        console.log(`transferOwnership transaction block hash: ${txReceipt.receipt.blockHash}`);
     }
 }
