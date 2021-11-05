cc3cf2e7aeaf954273c80837c774c57692430053
Greg Solovyev <fiddlestring@gmail.com>
2021-11-05 12:13:34 -0700

    Add more logging to deploy_staking

--- migrations/4_deploy_staking.js
+++ migrations/4_deploy_staking.js
@@ -6,8 +6,16 @@ module.exports = async function(deployer, network, accounts) {
         const iHEODao = await HEODAO.deployed();
         await deployer.deploy(HEOStaking);
         const iStaking = await HEOStaking.deployed();
-        await iStaking.transferOwnership(iHEODao.address);
-        await iHEODao.setStaking(iStaking.address);
         console.log(`HEOStaking address on ${network}: ${iStaking.address}`);
+
+        let txReceipt = await iStaking.transferOwnership(iHEODao.address);
+        console.log(`transferOwnership transaction cost: ${txReceipt.receipt.gasUsed}`);
+        console.log(`transferOwnership transaction hash: ${txReceipt.receipt.transactionHash}`);
+        console.log(`transferOwnership transaction block hash: ${txReceipt.receipt.blockHash}`);
+
+        txReceipt = await iHEODao.setStaking(iStaking.address);
+        console.log(`setStaking transaction cost: ${txReceipt.receipt.gasUsed}`);
+        console.log(`setStaking transaction hash: ${txReceipt.receipt.transactionHash}`);
+        console.log(`setStaking transaction block hash: ${txReceipt.receipt.blockHash}`);
     }
 }
