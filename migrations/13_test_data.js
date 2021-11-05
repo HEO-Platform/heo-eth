a37dd61fabc3d0ebc4bab13eef03ec58bc61a133
Greg Solovyev <fiddlestring@gmail.com>
2021-11-05 12:18:04 -0700

    Add auroratest to test data migration

--- migrations/13_test_data.js
+++ migrations/13_test_data.js
@@ -104,5 +104,26 @@ module.exports = async function(deployer, network, accounts) {
         console.log(`Voted for proposal: ${proposalId}`);
         await iHEODao.executeProposal(proposalId, {from: accounts[1]});
         console.log(`Executed proposal: ${proposalId}`);
+    } else if (network == "auroratest") {
+        await deployer.deploy(StableCoinForTests, "TUSD");
+        const iTestCoin = await StableCoinForTests.deployed();
+
+        await iTestCoin.transfer(accounts[0], web3.utils.toWei("1000000"));
+        await iTestCoin.transfer(accounts[1], web3.utils.toWei("1000000"));
+        await iTestCoin.transfer(accounts[2], web3.utils.toWei("1000000"));
+
+        //add stable-coin to accepted currencies
+        const iHEODao = await HEODAO.deployed();
+        await iHEODao.proposeVote(1, 0, KEY_ACCEPTED_COINS, [iTestCoin.address], [1], 259201, 51,
+            {from: accounts[0]});
+        let events = await iHEODao.getPastEvents('ProposalCreated');
+        let proposalId = events[0].returnValues.proposalId;
+
+        await iHEODao.vote(proposalId, 1, ONE_COIN, {from: accounts[0]});
+        await iHEODao.vote(proposalId, 1, ONE_COIN, {from: accounts[1]});
+        await iHEODao.vote(proposalId, 1, ONE_COIN, {from: accounts[2]});
+        await iHEODao.executeProposal(proposalId, {from: accounts[1]});
+
+        console.log(`Stable coin address: ${iTestCoin.address}`);
     }
 }
