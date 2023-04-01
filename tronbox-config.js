7d719a5aa9e854433e28ff8658bd69c96cc35d73
Greg Solovyev <fiddlestring@gmail.com>
2023-04-01 15:23:43 -0700

    add shasta and celo files to gitignore

---
+++ tronbox-config.js
@@ -0,0 +1,61 @@
+const fs = require("fs");
+const port = process.env.HOST_PORT || 9090
+const pk = fs.readFileSync(".pk_shasta").toString().trim();
+const mnemonic = fs.readFileSync(".secret_testnet").toString().trim();
+module.exports = {
+  networks: {
+    mainnet: {
+      // Don't put your private key here:
+      privateKey: process.env.PRIVATE_KEY_MAINNET,
+      /*
+Create a .env file (it must be gitignored) containing something like
+
+  export PRIVATE_KEY_MAINNET=4E7FEC...656243
+
+Then, run the migration with:
+
+  source .env && tronbox migrate --network mainnet
+
+      */
+      userFeePercentage: 100,
+      feeLimit: 1000 * 1e6,
+      fullHost: 'https://api.trongrid.io',
+      network_id: '1'
+    },
+    shasta: {
+      mnemonic: mnemonic,
+      userFeePercentage: 50,
+      feeLimit: 1000 * 1e7,
+      fullHost: 'https://api.shasta.trongrid.io',
+      network_id: '2'
+    },
+    nile: {
+      privateKey: process.env.PRIVATE_KEY_NILE,
+      userFeePercentage: 100,
+      feeLimit: 1000 * 1e6,
+      fullHost: 'https://nile.trongrid.io',
+      network_id: '3'
+    },
+    development: {
+      // For tronbox/tre docker image
+      privateKey: '0000000000000000000000000000000000000000000000000000000000000001',
+      userFeePercentage: 0,
+      feeLimit: 1000 * 1e6,
+      fullHost: 'http://127.0.0.1:' + port,
+      network_id: '9'
+    },
+    compilers: {
+      solc: {
+        version: '0.6.12'
+      }
+    }
+  },
+  // solc compiler optimize
+  solc: {
+  //   optimizer: {
+  //     enabled: true,
+  //     runs: 200
+  //   },
+  //   evmVersion: 'istanbul'
+  }
+}
