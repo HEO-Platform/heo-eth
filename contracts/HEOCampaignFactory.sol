ac64d52b4de9fcbed9eba645aa2525bbe22578ab
Greg Solovyev <fiddlestring@gmail.com>
2024-01-07 15:35:15 -0800

    Remove campaign registration to reduce cost of creating a campaign

--- contracts/HEOCampaignFactory.sol
+++ contracts/HEOCampaignFactory.sol
@@ -7,13 +7,8 @@ import "openzeppelin-solidity/contracts/access/Ownable.sol";
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "openzeppelin-solidity/contracts/utils/ReentrancyGuard.sol";
 import "./HEOCampaign.sol";
-import "./IHEOPriceOracle.sol";
 import "./IHEOCampaignFactory.sol";
-import "./IHEOCampaignRegistry.sol";
-import "./IHEORewardFarm.sol";
-import "./HEOParameters.sol";
 import "./HEODAO.sol";
-import "./HEOLib.sol";
 
 contract HEOCampaignFactory is IHEOCampaignFactory, Ownable, ReentrancyGuard {
     using SafeMath for uint256;
@@ -29,7 +24,7 @@ contract HEOCampaignFactory is IHEOCampaignFactory, Ownable, ReentrancyGuard {
     );
 
     constructor (HEODAO dao) public {
-        require(address(dao) != address(0), "HEOCampaignFactory: DAO cannot be zero-address");
+        require(address(dao) != address(0));
         _dao = dao;
     }
 
@@ -39,17 +34,15 @@ contract HEOCampaignFactory is IHEOCampaignFactory, Ownable, ReentrancyGuard {
     */
     function createCampaign(uint256 maxAmount, address payable beneficiary, string memory metaData) external override nonReentrant {
         HEOCampaign campaign = new HEOCampaign(maxAmount, beneficiary, _dao, 0, 0, 0, 0, 0, address(0), metaData);
-        IHEOCampaignRegistry registry = IHEOCampaignRegistry(_dao.heoParams().contractAddress(HEOLib.CAMPAIGN_REGISTRY));
         campaign.transferOwnership(_msgSender());
-        registry.registerCampaign(address(campaign));
         emit CampaignDeployed(address(campaign), address(_msgSender()), beneficiary, maxAmount);
     }
-    
+
     /*
     * Override default Ownable::renounceOwnership to make sure
     * this contract does not get orphaned.
     */
     function renounceOwnership() public override {
-        revert("HEOCampaignFactory: Cannot renounce ownership.");
+        revert();
     }
 }
