6f7477fb6e8786c4f1733dae6c2bba21871f2982
Greg Solovyev <fiddlestring@gmail.com>
2024-01-07 15:18:21 -0800

    Remove unused imports of IHEOCampaignRegistry

--- contracts/HEOSale.sol
+++ contracts/HEOSale.sol
@@ -9,7 +9,6 @@ import "openzeppelin-solidity/contracts/token/ERC20/SafeERC20.sol";
 import "openzeppelin-solidity/contracts/utils/ReentrancyGuard.sol";
 import "./IHEOPriceOracle.sol";
 import "./IHEOCampaign.sol";
-import "./IHEOCampaignRegistry.sol";
 import "./HEODAO.sol";
 import "./HEOLib.sol";
 
